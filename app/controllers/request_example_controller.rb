591f3c08fe35bb9eca83f158b6a306c7ac58232a
John Doe <johndoe@example.com>
2022-05-05 16:46:30 +0800

    3

--- app/controllers/request_example_controller.rb
+++ app/controllers/request_example_controller.rb
@@ -2,11 +2,11 @@ class RequestExampleController < ApplicationController
     skip_before_action :verify_authenticity_token
     
   def curl_get_example
-    render text: 'Thanks for sending a GET request with cURL!',layout:false
+    render plain: 'Thanks for sending a GET request with cURL!',layout:false
   end
 
   def curl_post_example
-    render text: "Thanks for sending a POST request with cURL!",layout:false
+    render plain: "Thanks for sending a POST request with cURL!",layout:false
   end
 end
 
